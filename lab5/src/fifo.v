9193cc6552533bd49d9203f6a63669aed070cba4
pipicaca123 <u9593103@gmail.com>
2024-07-24 12:37:16 +0800

    fifo done

--- lab5/src/fifo.v
+++ lab5/src/fifo.v
@@ -1,3 +1,5 @@
+// STATE MACHINE WILL BE SMARTER
+// TODO: simutanous read/write sim
 module fifo #(
     parameter WIDTH = 8,
     parameter DEPTH = 32,
@@ -15,7 +17,35 @@ module fifo #(
     output [WIDTH-1:0] dout,
     output empty
 );
-    assign full = 1'b1;
-    assign empty = 1'b0;
-    assign dout = 0;
+
+    reg [POINTER_WIDTH-1:0]read_ptr;
+    reg [POINTER_WIDTH-1:0]write_ptr;
+    reg [POINTER_WIDTH:0] bit_level;
+    reg [WIDTH-1:0] dout_reg;
+    reg [WIDTH-1:0] mem_buffer[DEPTH-1:0];
+
+    assign empty = bit_level == 0;
+     /* verilator lint_off WIDTH */
+    assign full = bit_level == DEPTH;
+     /* lint_on  */
+    assign dout = dout_reg;
+
+    always @(posedge clk) begin
+        if(rst)begin
+            bit_level <= 0;
+            read_ptr <= 0;
+            write_ptr <= 0;
+        end
+        if(wr_en && !full)begin
+            write_ptr <= write_ptr + 1;
+            bit_level = bit_level + 1; // TODO: better discription?
+            mem_buffer[write_ptr] <= din;
+        end
+        if(rd_en && !empty)begin
+            read_ptr <= read_ptr + 1;
+            bit_level = bit_level - 1; // TODO: better discription?
+            dout_reg <= mem_buffer[read_ptr];
+        end
+    end
+
 endmodule
